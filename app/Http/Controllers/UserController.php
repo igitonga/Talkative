d1182cffe48863af94099cf49324aef8cab24fb5
igitonga <iangitonga47@gmail.com>
2024-01-02 12:22:39 +0300

    Sign up and login api

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+use App\Models\User;
+use DB;
+
+class UserController extends Controller
+{
+    public function __construct()
+    {
+        # By default we are using here auth:api middleware
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+
+    protected function respondWithToken($token)
+    {
+        # This function is used to make JSON response with new
+        # access token of current user
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function create(Request $request){
+        try{
+            DB::beginTransaction();
+
+            $user = new User;
+            $user->first_name = $request->firstName;
+            $user->last_name = $request->lastName;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            DB::commit();
+            return \response([
+                'status' => Response::HTTP_OK,
+                'message' => 'User registered successfully'
+            ]);
+        }
+        catch(Exception $e){
+            return \response([
+                'status' => Response::HTTP_NOT_FOUND,
+                'message' => $e->getMessage(),
+            ]);
+        }
+    }
+
+    public function login()
+    {
+        try{
+            $credentials = request(['email', 'password']);
+
+            if (! $token = auth()->attempt($credentials)) {
+                return response([
+                    'status' => Response::HTTP_UNAUTHORIZED,
+                    'message' => 'Unauthorized',
+                ]);
+            }
+    
+            return $this->respondWithToken($token);
+        }
+        catch(Exception $e){
+            return \response([
+                'status' => Response::HTTP_NOT_FOUND,
+                'message' => $e->getMessage(),
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('create-user',[UserController::class, 'create']);
+
+Route::group(['middleware' => 'api','prefix' => 'auth'], function ($router) {
+    Route::post('login', [UserController::class, 'login']);
 });
