a03de530aa07f02323c643e206695a060a9a89c7
igitonga <iangitonga47@gmail.com>
2024-07-04 16:50:50 +0300

    Fix: added maintenance mode check

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Variable name to check maintenance mode
+ENV_VAR_NAME="MAINTENANCE_MODE"
+
+# Check if the environment variable is set to "true"
+if [[ "${!ENV_VAR_NAME}" = "true" ]]; then
+  echo "Entering maintenance mode..."
+  php artisan down
+fi
+
 # Install composer dependencies without dev dependencies
 composer install --optimize-autoloader --no-dev
 
@@ -13,4 +22,10 @@ php artisan optimize:clear
 php artisan passport:install
 
 # Database migration
-php arisan migrate --force
+php artisan migrate --force
+
+# Check if the environment variable is set to "false" or not set at all
+if [[ "${!ENV_VAR_NAME}" = "false" ]] || [[ -z "${!ENV_VAR_NAME}" ]]; then
+  echo "Exiting maintenance mode..."
+  php artisan up
+fi
